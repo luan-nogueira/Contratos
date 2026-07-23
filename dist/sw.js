15282a9d077575ed4b1af598a229d2372b1f68aa
Bravus Barbearia <admin@bravus.com>
2026-07-23 14:46:43 -0300

    fix: suporte a dist e raiz para deploy estatico no Vercel

---
+++ dist/sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'digital-contratos-v5';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/logo-digital.png',
+  '/logo-emive.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
